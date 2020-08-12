e6917ae4cfa11b5e78cde2caf3a4f4a22af89760
Nadiia <nenadinweb@gmail.com>
2020-08-12 22:02:29 +0300

    async generator was added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,49 @@
 //= partials/helper.js
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('Hello');
-  });
+async function* fetchCommits() {
+  let url = `https://jsonplaceholder.typicode.com/comments?_limit=5`;
+  console.log('url = ', url);
+  //while (url) {
+  //
+  //}
+const response = await fetch(url);
+    const commits = await response.json();
+    //console.log("commits =", commits);
+    //const currentPostCommits = [...commits].filter(commit => commit.postId = id);
+    //console.log("currentPostCommits =", currentPostCommits);
+    for (let commit of commits) {
+      yield commit
+    }
+}
+
+(async () => {
+  let count = 0;
+  for await (const commit of
+  fetchCommits()
+  )
+  {
+    console.log('commit.email = ', commit.email);
+  }
+  console.log("count =", count);
+  if (++count == 10) { // остановимся на 100 коммитах
+    return;
+  }
+})();
+
+//document.addEventListener("DOMContentLoaded", function () {
+//  printNumbers(2, 10);
+//});
 
 
-
+//let count = 0;
+//for (const commit of
+//  fetchCommits()
+//  ) {
+//  console.log('commit.email = ', commit.email);
+//}
+//if (++count == 10) { // остановимся на 100 коммитах
+//  break;
+//}
 
 
 
